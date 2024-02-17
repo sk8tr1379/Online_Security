f91e95a36c95798949f28aa766e5b74375be5a94
sk8tr1379 <159780057+sk8tr1379@users.noreply.github.com>
2024-02-17 08:27:40 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -51,8 +51,8 @@ const helmet = require('helmet');
 
 module.exports = app;
 const api = require('./server.js');
-app.use(helmet.frameguard({action: 'deny'}));
 app.use(express.static('public'));
+app.use(helmet.frameguard({action: 'deny'}));
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
