2655956384a6673e070f7bdcdfa516ba22a4f8c1
sk8tr1379 <159780057+sk8tr1379@users.noreply.github.com>
2024-02-17 08:41:29 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -59,11 +59,11 @@ app.use(helmet.xssFilter());
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.use(helmet.noSniff());
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+app.use(helmet.noSniff());
 app.use(helmet.hidePoweredBy());
 
 
