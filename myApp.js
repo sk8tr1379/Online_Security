3c3b3f8b5d124fc5f7f4d8fd6f51e16332ebbfea
sk8tr1379 <159780057+sk8tr1379@users.noreply.github.com>
2024-02-17 08:12:40 -0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -62,4 +62,4 @@ app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action:'deny'}));
+app.use(helmet.frameguard({action: 'deny'}));
